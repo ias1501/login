606306217453c87f9783b926469fe8b9ead41dba
ias1501 <103571149+ias1501@users.noreply.github.com>
2023-02-04 21:34:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="mb-0">
+        <h1>SIGN IN </h1>
+      </div>
+      <div className="mb-1">
+        <h2>
+          Sign in now and register for upcoming events and future announcements.
+        </h2>
+      </div>
+
+      <div className="mb-3"></div>
+
+      <div className="mb-2">
+        <h3>Email</h3>
+      </div>
+
+      <div className="mb-3"></div>
+
+      <div className="mb-4"></div>
+
+      <div className="mb-5">
+        <a href="https://mail.google.com/mail/u/0/#inbox"> Get Link </a>
+      </div>
+
+      <div className="mx-3"></div>
+      <div className="mt-2"></div>
+
+      <div className="mt-3"></div>
+
+      <div className="mx-4"></div>
+      <div className="ml-5"></div>
+      <div className=""></div>
+    </div>
+  );
+}
